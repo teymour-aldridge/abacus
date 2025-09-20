badefd666af28b00e2ae63b8cf49a7646077290b
Teymour Aldridge <teymour@reasoning.page>
2025-09-20 23:01:48 +0100

    Short docstring update.

--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,1 +1,6 @@
+//! This module contains user interface components which are commonly reused
+//! between different parts of the user interface.
+//!
+//! Currently it just contains the [`alert`] module (but more will come).
+
 pub mod alert;
